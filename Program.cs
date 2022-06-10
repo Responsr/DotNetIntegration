1b047accdb4bd42f66ffdaf3028e4b0f9daeda93
Magnus Olander <magnus@olander.nu>
2022-06-10 14:08:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,168 @@
+﻿using Responsr.Partner.Integration;
+using Responsr.Partner.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+/* 
+ *  -------------------------------------------------------- 
+ *  Responsr Integration template project V1.01
+ *  --------------------------------------------------------
+ *  
+ *  TL;DR:
+ *  
+ *  - Fetch data from your system and send it to Responsr
+ *  - Fetch only new posts that have not already been sent
+ *  - Fetch only for clients already activly using Responsr 
+ *  
+ *  Add NUGET Source: 
+ *  https://qsdev.pkgs.visualstudio.com/ResponsrPublic/_packaging/ResponsrPartner/nuget/v3/index.json
+ *  
+ *  Add Nuget packages:
+ *  Responsr.Partner.Integration
+ *  Microsoft.Extensions.Logging.Abstractions
+ *  Newtonsoft.Json
+ *  
+ *  - Sample code below
+ *  
+ *  Additional information in README.txt
+ *  
+ */
+
+namespace SendToResponsr
+{
+    class Program
+    {
+        private static Logger _logger = null;
+        private static bool _debug = false;
+
+        public static void Debug(string text)
+        {
+            if (_debug) { Console.WriteLine(text); }
+        }
+
+        static void Main(string[] args)
+        {
+            // Create somewhere to log and check for tries to debug the application
+            _logger = new Logger(ConfigurationManager.AppSettings["LogFolderPath"]);
+            _debug = (args.Contains("help") || args.Contains("debug") || args.Contains("verbose") || ConfigurationManager.AppSettings["Debug"].ToLower == "true") ? true : false;
+
+            // Create a batchrunner to send data to Responsr, this part is standard
+            var batchRunner = new ProductionBatchRunner(new BatchRetriever(), new Guid(ConfigurationManager.AppSettings["PartnerKey"]), ConfigurationManager.AppSettings["LogFolderPath"]);
+            batchRunner.RunBatch();
+
+            Debug("Application finished");
+        }
+
+        private class BatchRetriever : IBatchRetriever
+        {
+            public PartnerIntegrationBatchModel RetrieveBatch(DateTime recommendedBatchRetrievalTimestamp, List<PartnerClientInfoModel> clients, List<PartnerVariableInfoModel> variables)
+            {
+                /*          -----------------------------------------------------------------------------------------
+                 *          THIS IS A SAMPLE IMPLEMENTATION OF INTEGRATION TO YOUR SYSTEM
+                 *          Your code goes here, this is fetching the data that should be sent to Responsr.
+                 *          In this sample we have made a mock up SQL query, your application might be using an API. 
+                 *          -----------------------------------------------------------------------------------------
+                 */
+
+                // Connect to mockup database
+
+                using var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Database"].ConnectionString);
+                connection.Open();
+
+                // Gets a list of all clients currently using Responsr, this is populated by responsr
+                // We will not send data to clients who are using Responsr in demo mode only.
+                var responsrClients = new List<PartnerBatchClientModel>();
+                foreach (var client in clients.Where(x => !x.IsInDemoMode)) {
+                    var responsrClient = new PartnerBatchClientModel { ClientId = client.ClientId, Persons = new List<PersonInBatchModel>() };
+
+                    _logger.LogInformation("Found client: " + client.ClientId);
+
+                    // ----------------------------------
+                    // client.ClientId
+                    // ----------------------------------
+                    //      The clientId of the client in Responsr. This is likely an integration key or similar provided by you
+                    //      Make sure these are NOT easily guessed or incorrectly entered such as number series (1,2,3,4 .. ) 
+                    //      The clientId is entered by the customer into Responsr and it is likely provided by your organisation through support or a UI
+
+                    // ----------------------------------
+                    // recommendedBatchRetrievalTimestamp
+                    // ----------------------------------
+                    //      Responsr will keep track of the last time you sent data and provide you with a suggested time to fetch changes from based on it.
+                    //      You can use this to fetch only new posts. this value is then set further down in the code to be used on your next execution of this
+                    //      transfer. The reccomended time will at most be a few days back when you get started. We don't want to send surveys to end customers
+                    //      that have not recently been in contact with your client.
+                    //
+                    //      Stored in UTC time within Responsr
+
+                    // Responsr will make sure that each end customer will only recieve a survey at most once every 30 days. That way we do not spam them.
+                    // please send ALL transactions to responsr since they will be used to keep track of how active the end customer is.
+
+                    // ----------------------------------
+                    // YOUR CODE GOES HERE : 
+                    // ----------------------------------
+                    /*
+                    var bookings = connection.Query<dynamic>(
+                        @"
+                            select FirstName, LastName, Email, Price, priceplan, CampaignName, Room, CheckinDate, CheckoutDate, BookingTime, BookingReference
+                            from Bookings where CustomerId = @clientId and CheckoutDate > @recommendedBatchRetrievalTimestamp
+                        ",
+                        new { clientId = client.ClientId, recommendedBatchRetrievalTimestamp }
+                    );
+
+                    foreach (var booking in bookings) {
+
+                        _logger.LogInformation("Found booking: " + booking.BookingReference);
+
+                        // Add a person to be sent to responsr
+                        responsrClient.Persons.Add(new PersonInBatchModel {
+                            CreatedAt = booking.CheckoutDate,
+                            Variables = new Dictionary<string, string> {
+                                { "firstname", booking.FirstName },
+                                { "lastname", booking.LastName },
+                                { "email", booking.Email },
+                                { "price", booking.Price },
+                                { "pricePlan", booking.Priceplan },
+                                { "campaignname", booking.CampaignName },
+                                { "room", booking.Room },
+                                { "checkin", booking.CheckinDate.ToString() },
+                                { "checkout", booking.CheckoutDate.ToString() },
+                                { "bookingTime", booking.BookingTime.ToString() },
+                                { "bookingreference", booking.BookingReference },
+                            }
+                        });
+                    }
+                    */
+                    // ----------------------------------
+                    // END OF YOUR CODE
+                    // ----------------------------------
+
+                    // Add to the list of data to send to responsr
+                    responsrClients.Add(responsrClient);
+                }
+
+                // ----------------------------------------
+                // SUGGESTED NEXT START TIME TO FETCH DATA
+                // ----------------------------------------
+                //      Set a new suggested datetime for the next call. You will recieve this back from Responsr on the next time you 
+                //      Initiate your program, hence no need for you to keep track of this locally
+
+                //      This is what you will recieve as recommendedBatchRetrievalTimestamp 
+                //      the next time you run your application. In this case we use the checkoutDate of the last post we retrieved
+                var latestRetrievalTimestamp = responsrClients.SelectMany(x => x.Persons).Max(x => DateTime.Parse(x.Variables["CheckoutDate"]));
+
+                //      You should avoid using DateTime.Now as latestRetrievalTimestamp since new posts to be sent to Responsr might be created
+                //      during the time this application is running, after having retrieved data. In that case that data might be missed.              
+                //
+                // var latestRetrievalTimestamp = DateTime.Now;
+
+                // Send the retrieved data to Responsr
+                var result = new PartnerIntegrationBatchModel(latestRetrievalTimestamp, responsrClients);
+                return result;
+            }
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,135 @@
+
+
+Responsr Integration template project
+
+
+------------------------------------------------------------------------------------
+Version 1.01
+------------------------------------------------------------------------------------
+
+This template is created to make it very simple to integrate any application to Responsr.
+
+The sample application has some mockup code to show how this could work if you would 
+send bookings from a database, but the template works as well with API calls.
+
+It is created as a Console Application that can be triggered by a Scheduled Task.
+
+------------------------------------------------------------------------------------
+Context
+------------------------------------------------------------------------------------
+
+Responsr is a customer loyalty software. To work it needs customer transactions sent
+to it. It is created to work in a SaaS (Software as a Service) environment.
+
+Your clients have a user account in Responsr or they sign up to Responsr to get one. 
+When they do they will recieve an "app", a ready to use customer loyalty module 
+linked to your system.
+
+Within this app they can enter a "Client id" or an "Integration key" towards your system. 
+This is how we both keep track on who of your clients uses Responsr and who you will 
+send transactions for. 
+
+* We will provide you with a list of the active clients
+* We will provide you with information on your latest call, so you don't send the 
+  same data over and over again.
+* We will make sure the end customers are not spammed by information from Responsr
+
+------------------------------------------------------------------------------------
+First step
+------------------------------------------------------------------------------------
+
+Download the Nugetpackage. Add a new Nuget source in Visual Studio.
+The source URL is 
+https://qsdev.pkgs.visualstudio.com/ResponsrPublic/_packaging/ResponsrPartner/nuget/v3/index.json
+
+Install the following packages:
+*  Responsr.Partner.Integration
+*  Microsoft.Extensions.Logging.Abstractions
+*  Newtonsoft.Json
+
+------------------------------------------------------------------------------------
+Key concepts
+------------------------------------------------------------------------------------
+
+PartnerKey:		
+You recieve an integration key from us. Enter it into app.config
+This key is used by the application to list who of your clients are active and when
+sending data to us. The template application will do this automatically for you.
+
+ClientId:		
+A unique, not easily guessed, key that we both share to identify your different clients. 
+
+latestRetrievalTimestamp:
+The last time you sent data to Responsr. You only need to send newer transactions.
+
+When to send:
+You send data when something is finished, such as when someone checked out of a hotel,
+when they left the restaurant or when the customer service ticket is closed.
+
+------------------------------------------------------------------------------------
+What data and formats
+------------------------------------------------------------------------------------
+
+Each interaction is centered around a Person. This is the end Customer.
+Based on your application there are attributes that are relevant to send to us such
+as names, email, phone numbers etc.
+
+A person that interacted with a end customer is called an Agent. This can be a 
+sales representative or customer service employee.
+
+All attributes sent to Responsr are handled as string.
+
+The sample application has the following attributes:
+* firstname
+* lastname
+* email
+* price
+* pricePlan
+* campaignName
+* room
+* checkin
+* checkout
+* bookingTime
+* bookingReference
+
+Examples from Customer Service integration:
+* mobilePhone
+* resolutionTime
+* createdAt
+* referenceId
+* agentId
+* agentName
+* agentEmail
+* tags
+
+------------------------------------------------------------------------------------
+Debuging and logging
+------------------------------------------------------------------------------------
+
+The application will automatically log some events and there is a structure for easy
+debugging when running the application.
+
+LogFolderPath:	
+This is where the application stores the logs. This folder must exist or the 
+application will fail.
+
+Your own logs:
+Ofcourse you'll want to log some interactions to your system as well. 
+You can do that through:
+_logger.LogInformation("YOUR MESSAGE HERE");
+
+Debugging:
+The application has a debug setting in application settings, but will also react to
+tries to debugging it such as adding "help", "debug", "verbose" as arguments on
+the command line. 
+
+Your own Debug comments
+You can do this through
+Debug("YOUR MESSAGE HERE");
+
+
+------------------------------------------------------------------------------------
+VERSION HISTORY
+------------------------------------------------------------------------------------
+1.0		Initial version
+1.01	Added README, log and debug functionality
